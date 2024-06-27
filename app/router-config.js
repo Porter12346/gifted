077b2b6bb732d149f12d4d148ea184f8e75946f3
Porter Young <porteryoung2005@gmail.com>
2024-06-27 14:18:59 -0600

    first commit

---
+++ app/router-config.js
@@ -0,0 +1,22 @@
+import { AccountController } from "./controllers/AccountController.js";
+import { AuthGuard } from "./services/AuthService.js";
+import { Router } from "./utils/Router.js";
+
+
+export const router = new Router([
+  {
+    path: '',
+    controllers: [],
+    view: 'app/views/HomeView.html'
+  },
+  {
+    path: '#/account',
+    middleware: [AuthGuard],
+    controllers: [AccountController],
+    view: 'app/views/AccountView.html',
+  }
+])
+
+
+
+
